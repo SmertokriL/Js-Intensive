14a9648d6231d75acea9642d5fbad613e4854c59
SmertokriL <43453177+SmertokriL@users.noreply.github.com>
2020-12-17 20:32:32 +0300

    Update index.js

--- Js - intensive/lesson 1/task1/index.js
+++ Js - intensive/lesson 1/task1/index.js
@@ -2,7 +2,7 @@ let number = parseInt(prompt('Input a number'))
 let radix = prompt('Input a radix')
 
 if (isNaN(number)) {
-  alert('Error')
+  alert('NaN Error')
 } else {
   try {
     alert('Result: ' + number.toString(radix))
